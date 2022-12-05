0e04914d40f9680dcffedf0922cd74810958a717
MeryAmun <chebesiawah@gmail.com>
2022-12-05 20:02:17 +0100

    responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
-    extend: {
-      screens:{
-        'widescreen':{'raw': '(min-aspect-ratio:3/2)'},
-        'tallscreen':{'raw': '(min-aspect-ratio:1/2)'},
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
